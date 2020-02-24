a0374fb205c707e0a660302ea4ee8f4a5056cab4
Chun-Ching <asd456331@gmail.com>
2020-02-24 22:56:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import discord
 from discord.ext import commands
 import json
 import random
+import os
+from _ast import For
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='>')
 
 @bot.event
 async def on_ready():
@@ -24,21 +26,10 @@ async def on_member_remove(member):
     channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(F'{member} leave!') 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(F'{round(bot.latency*1000)} (ms)')
 
 
-@bot.command()
-async def 圖片(ctx):
-    random_pic = random.choice(jdata['pic'])
-    pic = discord.File(random_pic)
-    await ctx.send(file = pic)
 
-@bot.command()
-async def web(ctx):
-    random_pic = random.choice(jdata['url_pic'])
-    await ctx.send(random_pic)
+
 
 
 bot.run(jdata['TOKEN'])
