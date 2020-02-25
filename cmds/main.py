2121a6857f6dcd4ca38a125ca11033753a03450b
Chun-Ching <asd456331@gmail.com>
2020-02-25 14:06:28 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -9,5 +9,10 @@ class Main(Cog_Extension):
     async def ping(self, ctx):
         await ctx.send(F'{round(self.bot.latency*1000)} (ms)')
 
+    @commands.command()
+    async def hi(self, ctx):
+        await ctx.send('123456789')
+
+
 def setup(bot):
     bot.add_cog(Main(bot))    
