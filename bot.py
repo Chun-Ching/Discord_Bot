ae1a6acca1e65755c8791fec17ae9edc83d0d20b
Chun-Ching <asd456331@gmail.com>
2020-02-25 14:05:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import json
 import random
 import os
-from _ast import For
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -27,9 +26,28 @@ async def on_member_remove(member):
     await channel.send(F'{member} leave!') 
 
 
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(F'cmds.{extension}')
+    await ctx.send(F'Loaded {extension} done.')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(F'cmds.{extension}')
+    await ctx.send(F'Un - Loaded {extension} done.')
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(F'cmds.{extension}')
+    await ctx.send(F'Re - Loaded {extension} done.')
 
 
 
+for Filename in os.listdir('./cmds'):
+    if Filename.endswith('.py'):
+        bot.load_extension(F'cmds.{Filename[:-3]}')
 
 
-bot.run(jdata['TOKEN'])
+
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
