8c6c9c7c69ecd618032d6021e52dbee3c5421c44
Chun-Ching <asd456331@gmail.com>
2020-02-25 20:22:50 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,16 +14,7 @@ bot = commands.Bot(command_prefix='>')
 async def on_ready():
     print(">> Bot is online <<")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(int(jdata['Welcome_channel']))
-    await channel.send(F'{member} join!')
 
-
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(int(jdata['Leave_channel']))
-    await channel.send(F'{member} leave!') 
 
 
 @bot.command()
