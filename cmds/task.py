ad7ecd43c96c3e200e866e54451ec67694d90ab3
Chun-Ching <asd456331@gmail.com>
2020-02-27 17:11:47 +0800

    Update task.py

--- cmds/task.py
+++ cmds/task.py
@@ -7,20 +7,49 @@ class Task(Cog_Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        async def interval():
+        self.counter = 0
+
+        #async def interval():
+        #    self.channel = self.bot.get_channel(680324588441567236)
+        #    while not self.bot.is_close():
+        #        await self.channel.send("Hi im running")
+        #        await asyncio.sleep(5)
+
+        #self.bg_task = self.bot.loop.create_task(interval())
+
+        async def time_task():
             await self.bot.wait_until_ready()
             self.channel = self.bot.get_channel(680324588441567236)
             while not self.bot.is_close():
-                await self.channel.send("Hi im running")
-                await asyncio.sleep(5)
+                now_time = datetime.datetime.now().strftime('%H%M')
+                with open('setting.json', 'r', encoding='utf8') as jfile:
+                    jdata = json.load(jfile)
+                if now_time == jdata['time'] and self.counter == 0:
+                    await self.channel.send('Task Working!')
+                    self.counter = 1
+                    await asyncio.sleep(1)
+                else:
+                    await asyncio.sleep(1)
+                    pass
+                        
 
-        self.bg_task = self.bot.loop.create_task(interval())        
+        self.bg_task = self.bot.loop.create_task(time_task())
 
 
     @commands.command()
     async def set_channel(self, ctx, ch:int):
         self.set_channel = self.bot.get_channel(ch)
         await ctx.send(f'Set Cgannel:{self.channel.mention}')
+
+    @commands.command()
+    async def set_time(self, ctx, time):
+        self.counter = 0
+        with open('setting.json', 'r', encoding='utf8') as jfile:
+            jdata = json.load(jfile)
+        
+        jdata['time'] = time
+        with open('setting.json', 'w', encoding='utf8') as jfile:
+            json.dump(jdata, jfile, indent=4)
 
 def setup(bot):
     bot.add_cog(Task(bot))
